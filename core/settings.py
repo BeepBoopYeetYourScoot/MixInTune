f51086719bd98b4a726a735b04c752577fd84389
kalitka <zavorotnii.dr@gmail.com>
2023-11-07 13:13:25 +0700

    Initial commit

---
+++ core/settings.py
@@ -0,0 +1,9 @@
+import dotenv
+
+dotenv.load_dotenv()
+
+
+SPOTIFY_CLIENT_ID = "56a7b64a311e40278ddcc00cc42e12b0"
+SPOTIFY_CLIENT_SECRET = "0004ba0c53344dd084e33b180ab533ff"
+SPOTIFY_SCOPES: list[str] = []
+SPOTIFY_REDIRECT_URL = "http://localhost:8080"
